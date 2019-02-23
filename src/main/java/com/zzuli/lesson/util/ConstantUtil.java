e6343ec0e6029c31d562578255d440e251df06e0
Administrator <Administrator@WINDOWS-D16AANP>
2019-02-23 13:02:02 +0800

    标题栏

--- src/main/java/com/zzuli/lesson/util/ConstantUtil.java
+++ src/main/java/com/zzuli/lesson/util/ConstantUtil.java
@@ -3,15 +3,15 @@ package com.zzuli.lesson.util;
 public final class ConstantUtil {
 
 	public interface FTP_PATH{
-		String FTP_BASEPATH = "\\LessonsUpload\\";
+		String FTP_BASEPATH = "\\LessonResource\\";
 	}
 	public interface SESSION_USER{
 		String SESSION_USER ="SESSION_USER";
 	}
 	public interface FTP_ADDRESS {
-		String FTP_ADDRESS = "172.18.255.26";
+		String FTP_ADDRESS = "10.84.143.249";
 	}
 	public interface FTP_PRE {
-		String FTP_PRE = "http://172.18.255.26:9090/LessonsUpload";
+		String FTP_PRE = "http://10.84.143.21:9090/LessonResource";
 	}
 }
