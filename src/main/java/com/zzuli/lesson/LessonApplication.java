6d116b9321ed99f608e90ad2bf7f4b6d96d2f49d
xukun3 <xukun3@asiainfo.com>
2019-03-07 20:27:08 +0800

    设置session有效时间为2小时

--- src/main/java/com/zzuli/lesson/LessonApplication.java
+++ src/main/java/com/zzuli/lesson/LessonApplication.java
@@ -3,7 +3,10 @@ package com.zzuli.lesson;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication //Spring Boot 应用的标识
@@ -13,4 +16,14 @@ public class LessonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LessonApplication.class, args);
 }
+	
+	@Bean 
+	public EmbeddedServletContainerCustomizer containerCustomizer(){ 
+	       return new EmbeddedServletContainerCustomizer() { 
+	@Override 
+	 public void customize(ConfigurableEmbeddedServletContainer container) { 
+	       container.setSessionTimeout(7200);//单位为S 
+	            } 
+	       }; 
+	   } 
 }
